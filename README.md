673793d66134a5710dba2d02371abbb51ab3942c
MixColumns <134201910+MixColumns@users.noreply.github.com>
2023-11-06 19:06:16 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # KaffeeKarte
+[https://coffee.subbytes.net/](https://coffee.subbytes.net/)
+
+# Used
+- [Roboto Font](https://fonts.google.com/specimen/Roboto)
+- [Leaflet](https://github.com/Leaflet/Leaflet)
+- [leaflet-color-markers](https://github.com/pointhi/leaflet-color-markers)
+- [Leaflet.MousePosition](https://github.com/ardhi/Leaflet.MousePosition)
---
+++ markers/markers.js
@@ -0,0 +1,109 @@
+markers=[{
+    "name": "Backwerk",
+    "icon": "green",
+    "latitud": 51.44856,
+    "longitud": 7.25821,
+    "description": "Kaffee aus dem Vollautomaten. Selbstbedienung, Morgens jedoch ggf. sehr voll."
+},{
+    "name": "Café Olé 🦊",
+    "icon": "violet",
+    "latitud": 51.44599,
+    "longitud": 7.26042,
+    "description": "Mo. - Fr. 8:00 - 17:00 Uhr . Im SSC direkt hinter dem Eingangsbereich. Zudem gibt es Brötchen, Toast, Kuchen und weiteres."
+},{
+    "name": "Kulturkaffee 🦊",
+    "icon": "violet",
+    "latitud": 51.44586,
+    "longitud": 7.25951,
+    "description": "Hat im Semester von Mo-Do 10-20 Uhr und Fr 10-16 Uhr geöffnet. Man kann sowohl drinnen als auch draußen sitzen. Es gibt zudem Eis, Kuchen, Pizza und weiteres. Schachbretter und Steckdosen sind auch vorhanden. <br><a href='https://kulturcafe.asta-bochum.de/' target='_blank'>Website</a>"
+},{
+    "name": "Q-West",
+    "icon": "green",
+    "latitud": 51.44409,
+    "longitud": 7.25883,
+    "description": "Besonders sind hier die legendären Erdbeertörtchen. Es gibt draußen und drinnen Sitzplätze. Vielleicht ein guter Ort für ein Date?"
+},{
+    "name": "Kaffeebar",
+    "icon": "green",
+    "latitud": 51.4433,
+    "longitud": 7.26224,
+    "description": "Kann je nach Uhrzeit sehr voll sein. Am besten einen Kaffe mit einer Kugel Vanilleeis im Becher bestellen und danach auf die Reling am Bistro setzen. Die Aussicht holt einiges wieder raus."
+},{
+    "name": "Edwards",
+    "icon": "green",
+    "latitud": 51.4452,
+    "longitud": 7.26054,
+    "description": "Praktischerweise direkt in der Bibliothek. Hat ein wenig Starbucks Vibes."
+},{
+    "name": "Kiosk Campus Center",
+    "icon": "green",
+    "latitud": 51.44392,
+    "longitud": 7.26006,
+    "description": "Kaffee vom Kiosk"
+},{
+    "name": "Beckmanns Hof",
+    "icon": "yellow",
+    "latitud": 51.44208,
+    "longitud": 7.26452,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier Kaffee?"
+},{
+    "name": "Botanischer Garten",
+    "icon": "yellow",
+    "latitud": 51.44303,
+    "longitud": 7.26689,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier Kaffee?"
+},{
+    "name": "SSC-Cafeteria",
+    "icon": "green",
+    "latitud": 51.4461,
+    "longitud": 7.26114,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen."
+},{
+    "name": "IB-Cafeteria",
+    "icon": "green",
+    "latitud": 51.44591,
+    "longitud": 7.26368,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen. Zudem gibt es Automaten."
+},{
+    "name": "ID-Cafeteria",
+    "icon": "green",
+    "latitud": 51.44703,
+    "longitud": 7.2663,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen. Zudem gibt es einen Automaten der auch spät noch Kaffee ausspuckt."
+},{
+    "name": "GA-Cafeteria",
+    "icon": "green",
+    "latitud": 51.44309,
+    "longitud": 7.25973,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen."
+},{
+    "name": "GD-Cafeteria",
+    "icon": "green",
+    "latitud": 51.44216,
+    "longitud": 7.25624,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen. Zudem gibt es Automaten."
+},{
+    "name": "NC-(Cafeteria)?",
+    "icon": "yellow",
+    "latitud": 51.4444,
+    "longitud": 7.26622,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier Kaffee?"
+},{
+    "name": "MA-(Cafeteria)?",
+    "icon": "yellow",
+    "latitud": 51.44463,
+    "longitud": 7.25923,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier Kaffee?"
+},{
+    "name": "HZO-Automat",
+    "icon": "green",
+    "latitud": 51.44492,
+    "longitud": 7.26226,
+    "description": "Im HZO stehen mehrere Automaten, jedoch sind diese auch die teuersten auf dem gesamten Campus."
+},{
+    "name": "Mensa Hochschule",
+    "icon": "yellow",
+    "latitud": 51.44628,
+    "longitud": 7.27175,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier Kaffee?"
+}];
